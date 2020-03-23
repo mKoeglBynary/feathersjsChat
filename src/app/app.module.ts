b558343d18a5009051b79bf64c79d3b33187a8b2
Maximilian Kögl <maximiliankoegl@bynarys-MacBook-Pro.local>
2020-03-23 09:48:06 +0100

    Moved Chat-Component in 'components' folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
-import { ChatComponent } from './chat/chat.component';
+import { ChatComponent } from './components/chat/chat.component';
 
 @NgModule({
   declarations: [
