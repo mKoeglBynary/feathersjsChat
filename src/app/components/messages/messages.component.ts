e3511267737f3342081c97191e3371812da85555
Maximilian Kögl <maximiliankoegl@bynarys-MacBook-Pro.local>
2020-04-08 13:24:33 +0200

    Added header for chat component

--- src/app/components/messages/messages.component.ts
+++ src/app/components/messages/messages.component.ts
@@ -1,22 +1,34 @@
 import {ChangeDetectionStrategy, Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
 import {Messages} from '../../interfaces/messages';
+import {newMessage} from '../../animations/newMessage';
 
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
   styleUrls: ['./messages.component.scss'],
+  animations: [
+    newMessage
+  ],
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.Default,
   host: {
+    '[@fadeIn]': '\'in\'',
+    '(@fadeIn.start)': 'toggle()',
+    '(@fadeIn.done)': 'toggle()',
     class: 'app-messages',
   }
 })
 export class MessagesComponent implements OnInit {
   @Input() messages: Messages[];
-
+  move = false;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggle() {
+    this.move = !this.move;
+    console.log('toggelin...')
   }
 
 }
