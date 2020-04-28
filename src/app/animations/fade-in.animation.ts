5433aab1fbc3f5e293d8607791ea8250fa6c6cdb
Maximilian Kögl <maximiliankoegl@bynarys-MacBook-Pro.local>
2020-04-28 11:21:48 +0200

    Made special const for styles for fade-ins

--- src/app/animations/fade-in.animation.ts
+++ src/app/animations/fade-in.animation.ts
@@ -1,6 +1,21 @@
 import {animate, AnimationTriggerMetadata, state, style, transition, trigger} from '@angular/animations';
 import {AnimationTransitions} from './animation';
 
+const fadeInStyles = {
+  hidden: {
+    opacity: 0,
+    visibility: 'hidden'
+  },
+  visible: {
+    opacity: 1,
+    visibility: 'visible',
+  },
+  transparent: {
+    opacity: 0,
+    visibility: 'visible'
+  }
+};
+
 export const fadeInAnimations: {
   readonly fadeIn: AnimationTriggerMetadata,
   readonly fadeInOverlay: AnimationTriggerMetadata,
@@ -8,32 +23,30 @@ export const fadeInAnimations: {
   } = {
   fadeIn: trigger('fadeIn', [
     transition(':enter', [
-      style({ opacity: 0}),
+      style(fadeInStyles.hidden),
       animate(AnimationTransitions.FADE_IN),
     ]),
   ]),
   fadeInOverlay: trigger('fadeInOverlay', [
     transition('* => true', [
       animate(AnimationTransitions.OVERLAY_IN_OUT, style({
-        opacity: 0,
-        visibility: 'visible',
+        ...fadeInStyles.transparent,
         transform: 'translateY(-150px) scale(0)'
       })),
       animate(AnimationTransitions.OVERLAY_SHOW, style({
-        opacity: 1,
+        ...fadeInStyles.visible,
         transform: 'translateY(200px)'
       })),
       animate(AnimationTransitions.OVERLAY_IN_OUT, style({
-        visibility: 'hidden',
-        opacity: 0,
-        transform: 'rotate(-90deg) translateY(-100px) scale(0.3)',
+        ...fadeInStyles.hidden,
+        transform: 'translateY(-100px) scale(0.3)',
       })),
     ])
   ]),
 
   fadeInAfter: trigger('fadeInAfter', [
-    state('false', style({opacity: 0, visibility: 'hidden'})),
-    state('true', style({opacity: 1, visibility: 'visible'})),
+    state('false', style(fadeInStyles.hidden)),
+    state('true', style(fadeInStyles.visible)),
     transition('false => true', [
       animate(AnimationTransitions.DEFAULT)
     ])
